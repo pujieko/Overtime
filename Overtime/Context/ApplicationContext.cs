933fcec8660fbd8d6412ddb7544b9f30f4a9df70
Eko Puji Hatmoko <eko.november06@gmail.com>
2019-07-24 09:03:15 +0700

    Intial Commit

---
+++ Overtime/Context/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overtime.Context
+{
+    class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("Overtime") { }
+    }
+}
