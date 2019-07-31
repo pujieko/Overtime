f2cccca1dd12759214fc0f868e807aaf82d1b4dc
ssatyaji <msatyaji31@gmail.com>
2019-07-31 08:57:40 +0700

    FIX BUG Overtime csproj

---
+++ Overtime/Controllers/ApprovementsController.cs
@@ -0,0 +1,47 @@
+﻿using Overtime.Models;
+using Overtime.Repositories;
+using Overtime.Repositories.Interfaces;
+using Overtime.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overtime.Controllers
+{
+    public class ApprovementsController
+    {
+        IApprovementRepository iApprovementRepository = new ApprovementRepository();
+
+        public List<Approvement> Get()
+        {
+            return iApprovementRepository.Get();
+        }
+
+        public Approvement Get(int id)
+        {
+            return iApprovementRepository.Get(id);
+        }
+
+        public List<Approvement> Get(string value)
+        {
+            return iApprovementRepository.Get(value);
+        }
+
+        public bool Insert(ApprovementVM approvementVM)
+        {
+            return iApprovementRepository.Insert(approvementVM);
+        }
+
+        public bool Update(int id, ApprovementVM approvementVM)
+        {
+            return iApprovementRepository.Update(id, approvementVM);
+        }
+
+        public bool Delete(int id)
+        {
+            return iApprovementRepository.Delete(id);
+        }
+    }
+}
